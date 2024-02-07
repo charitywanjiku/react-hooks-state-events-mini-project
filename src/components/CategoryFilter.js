1f86aad6b848dad31f0619dde9a88da359195c16
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-02-07 22:14:52 +0300

    assignment done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
-}
 
+function CategoryFilter({ categories, categoryIndex, onSelectedButtonChange }) {
+    const buttons = categories.map((category, index) => {
+      const selectedClass =
+        categoryIndex === index ? { className: "selected" } : {};
+      return (
+        <button
+          {...selectedClass}
+          key={category}
+          onClick={() => onSelectedButtonChange(index)}
+        >
+          {category}
+        </button>
+      );
+    });
+    return (
+      <div className="categories">
+        <h5>Category filters</h5>
+        {buttons}
+      </div>
+    );
+  }
 export default CategoryFilter;
